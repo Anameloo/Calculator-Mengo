58efa9b6f3c836b78f9cae1f0035e3b9977253b0
Ana Melo <anamelo.ac9@gmail.com>
2025-05-08 08:28:17 -0300

    Corrige para parar o som só ao apertar o botão C

--- script.js
+++ script.js
@@ -2,11 +2,19 @@
 function adicionar(valor) {
     const display = document.getElementById('display');
     display.value += valor; // Adiciona o valor no final do texto atual
-  }
+  
+
+}  
   
   // Função para limpar o visor
   function limpar() {
     document.getElementById('display').value = '';
+
+
+    // Parar o som ao limpar
+    const audio = document.getElementById('audioGol');
+    audio.pause();
+    audio.currentTime = 0;
   }
   
   // Função para calcular o resultado
@@ -15,7 +23,13 @@ function adicionar(valor) {
       const display = document.getElementById('display');
       // Usa a função eval para calcular a expressão matemática digitada
       display.value = eval(display.value);
+
+      const audio = document.getElementById('audioGol');
+    audio.play();
+      
+
     } catch (e) {
+     
       alert('Expressão inválida!');
     }
   }
